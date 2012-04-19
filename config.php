64876cbdc716fa56f7dcfe7be8d00de51ffffc57
ringmaster <a_github@midnightcircus.com>
2012-04-19 10:18:55 -0400

    Add my config for orchestra.io.  Yours will be different.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); }
+Config::set( 'db_connection', array(
+	'connection_string'=>'mysql:host=a.db.shared.orchestra.io;dbname=db_0b3f1679',
+	'username'=>'user_0b3f1679',
+	'password'=>'O)x6ju_7eaU.Fg',
+	'prefix'=>''
+));
+define('DEBUG', true);
+// $locale = 'en-us';
+?>
